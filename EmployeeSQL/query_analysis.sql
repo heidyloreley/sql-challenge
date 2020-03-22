37594f0328c5453192ecc2906477f4af0ef041b0
heidyloreley <59861439+heidyloreley@users.noreply.github.com>
2020-03-22 00:27:51 -0600

    correct file

--- EmployeeSQL/query_analysis.sql
+++ EmployeeSQL/query_analysis.sql
@@ -48,8 +48,19 @@ from departments d
 join dep_employees on d.dept_no = dep_employees.dept_no
 join employees as e on e.emp_no = dep_employees.emp_no
 join salaries on e.emp_no = salaries.emp_no
+where dept_name = 'Sales';
 
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, dept_name
+from departments d
+join dep_employees on d.dept_no = dep_employees.dept_no
+join employees as e on e.emp_no = dep_employees.emp_no
+join salaries on e.emp_no = salaries.emp_no
+where dept_name ='Sales' or dept_name ='Development'
+order by dept_name;
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select last_name, count(*) from employees
+group by last_name
+order by last_name desc;
